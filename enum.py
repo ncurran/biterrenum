8f602851910957a9c81daddfce5232c15ae63cf7
user <nicholas@curran.tech>
2018-05-31 00:22:58 +0000

    new file:   enum.py

---
+++ enum.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import binascii
+import sys
+
+def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
+    bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
+    return bits.zfill(8 * ((len(bits) + 7) // 8))
+
+def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
+    n = int(bits, 2)
+    return int2bytes(n).decode(encoding, errors)
+
+def int2bytes(i):
+    hex_string = '%x' % i
+    n = len(hex_string)
+    return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
+
+before = sys.argv[1]
+binary = text_to_bits(before)
+
+i = 0
+for char in text_to_bits(before):
+    error = text_to_bits(before)
+    error = [char for char in error]
+    if error[i] == '0':
+        error[i] = '1'
+    if error[i] == '1':
+        error[i] = '0'
+    error = ''.join([char for char in error])
+    #print( '%s'%(error) )
+    error = text_from_bits(error)
+    print('%s'%error)
+    i+=1
